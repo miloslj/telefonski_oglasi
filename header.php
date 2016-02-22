013f704e2e5a0e7457483f9311ae5c5bf137153b
gdjordjevic <gdjordjevic@live.com>
2016-02-22 02:32:36 +0100

    Header sa pozivom fajlova, bez svih funkcionalnosti

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+	<title>telefonski_oglasi</title>
+	<!-- Stylesheet -->
+	<?php get_css(); ?>
+</head>
+<body>
+	<header id="main" role="Page Header">
+		<img id="logo" role="Logo" src="img/logo.png" />
+		<form id="search" role="Search" action="search.php" method="GET">
+			<input type="text" name="search-term" role="Search box" id="search-box">
+			<input type="submit" value=" ">
+		</form>
+		<section id="users" role="Log in or Register">
+			<div class="avatar"</div>
+		</section>
+	</header>
